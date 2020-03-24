721059aac0b5036c86cc953f865c5ba5d07dc239
jp <jp.ehret@laposte.net>
2020-03-24 20:16:53 +1300

    Conteneur léger

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -10,6 +10,8 @@ import com.mycompany.invoise.repository.InvoiceRepositoryDatabase;
 import com.mycompany.invoise.service.InvoiceServiceInterface;
 import com.mycompany.invoise.service.InvoiceServiceNumber;
 import com.mycompany.invoise.service.InvoiceServicePrefix;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
@@ -22,33 +24,8 @@ public class App
     public static void main( String[] args )
     {
 
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Quel est la classe de controller  ?");
-        String controllerClass=sc.nextLine();
-        System.out.println("Quel est la classe de service ?");
-        String serviceClass=sc.nextLine();
-        System.out.println("Quel est la classe repository ?");
-        String repositoryClass=sc.nextLine();
-
-
-        InvoiceControllerInterface invoiceController=null;
-
-        InvoiceServiceInterface invoiceService=null;
-
-        InvoiceRepositoryInterface invoiceRepository=null;
-
-        try {
-            invoiceController = (InvoiceControllerInterface) Class.forName(controllerClass).getDeclaredConstructor().newInstance();
-            invoiceService = (InvoiceServiceInterface) Class.forName(serviceClass).getDeclaredConstructor().newInstance();
-            invoiceRepository = (InvoiceRepositoryInterface) Class.forName(repositoryClass).getDeclaredConstructor().newInstance();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-        invoiceController.setInvoiceService(invoiceService);
-        invoiceService.setInvoiceRepository(invoiceRepository);
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        InvoiceControllerInterface invoiceController=context.getBean(InvoiceControllerInterface.class);
 
         invoiceController.createInvoice();
     }
