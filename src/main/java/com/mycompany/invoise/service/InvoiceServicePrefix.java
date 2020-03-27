fa7c739a7a00a16ec22f53a172372b7c7e06910a
jp <jp.ehret@laposte.net>
2020-03-27 20:00:43 +1300

    Annotation property value

--- src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -3,11 +3,14 @@ package com.mycompany.invoise.service;
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface{
 
+    @Value("${invoice.lastNumber}")
     private long lastNumber;
+    @Value("${invoice.prefix}")
     private String prefix;
 
     @Autowired
