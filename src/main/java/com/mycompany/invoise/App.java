a0d9e4209211a9e1130fdb6293907782a653c1ea
jp <jp.ehret@laposte.net>
2020-04-14 10:49:06 +1200

    Configuration beans in details

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,23 +1,33 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.service.InvoiceServiceInterface;
+import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.invoise.controller.web"/*,"com.mycompany.invoise.service.prefix"*/,"com.mycompany.invoise.repository.database"})
+@PropertySource("classpath:application.properties")
 public class App
 {
     public static void main( String[] args )
     {
 
         //ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceController=context.getBean(InvoiceControllerInterface.class);
 
         invoiceController.createInvoice();
+    }
+
+    @Bean
+    public InvoiceServiceInterface configureInvoiceService(){
+        return new InvoiceServicePrefix();
     }
 }
