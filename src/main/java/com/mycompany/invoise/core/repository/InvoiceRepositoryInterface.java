cc5cab8c0f006bfd7b0e7687c5b0fb4cfe7e0335
jp <jp.ehret@laposte.net>
2020-06-17 14:08:44 +1200

    oubli graph

--- src/main/java/com/mycompany/invoise/core/repository/InvoiceRepositoryInterface.java
+++ src/main/java/com/mycompany/invoise/core/repository/InvoiceRepositoryInterface.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface InvoiceRepositoryInterface extends CrudRepository<Invoice,String> {
+    @EntityGraph(value="invoice.customer",type= EntityGraph.EntityGraphType.FETCH)
+    Iterable<Invoice> findAll();
     /*Invoice create(Invoice invoice);
     List<Invoice> list();
     Invoice getById(String number);*/
