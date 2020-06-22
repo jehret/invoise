c375eac33a2500843730b9bf92143a5365c08137
jp <jp.ehret@laposte.net>
2020-06-22 15:43:35 +1200

    wrong commit

--- src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
+++ src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
@@ -7,7 +7,6 @@ import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -37,7 +36,6 @@ public class InvoiceServiceNumber implements InvoiceServiceInterface {
         this.invoiceRepository = invoiceRepository;
     }
 
-    @Transactional
     public Invoice createInvoice(Invoice invoice){
         customerRepository.save(invoice.getCustomer());
         return invoiceRepository.save(invoice);
