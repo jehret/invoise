6205ac667364dd40cf4fb4321630444306c02ab0
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-06 11:47:50 +0200

    format code propre

--- src/main/java/com/mycompany/invoise/repository/database/InvoiceRepositoryDatabase.java
+++ src/main/java/com/mycompany/invoise/repository/database/InvoiceRepositoryDatabase.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class InvoiceRepositoryDatabase implements InvoiceRepositoryInterface {
 
-    public void create(Invoice invoice){
+    public void create(Invoice invoice) {
         /*[…]
         PreparedStatement pstmt=connexion.prepareStatement("INSERT INTO FACTURE (NUMBER,CUSTOMERNAME) VALUES (?,?)");
                 […]
         pstmt.executeUpdate();*/
-        System.out.println("Database: Invoice added with number "+invoice.getNumber()+" for "+invoice.getCustomerName());
+        System.out.println("Database: Invoice added with number " + invoice.getNumber() + " for " + invoice.getCustomerName());
     }
 
 }
--- src/main/java/com/mycompany/invoise/service/number/InvoiceServiceNumber.java
+++ src/main/java/com/mycompany/invoise/service/number/InvoiceServiceNumber.java
@@ -1,15 +1,13 @@
 package com.mycompany.invoise.service.number;
 
 import com.mycompany.invoise.entity.Invoice;
-
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoise.service.InvoiceServiceInterface;
-import org.springframework.stereotype.Service;
 
 //@Service
 public class InvoiceServiceNumber implements InvoiceServiceInterface {
 
-    private static long lastNumber=0L;
+    private static long lastNumber = 0L;
 
     private InvoiceRepositoryInterface invoiceRepository;
 
@@ -21,7 +19,7 @@ public class InvoiceServiceNumber implements InvoiceServiceInterface {
         this.invoiceRepository = invoiceRepository;
     }
 
-    public void createInvoice(Invoice invoice){
+    public void createInvoice(Invoice invoice) {
         invoice.setNumber(String.valueOf(++lastNumber));
         invoiceRepository.create(invoice);
     }
