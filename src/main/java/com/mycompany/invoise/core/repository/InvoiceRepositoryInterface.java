f1e1617ccb1ba8aad99804b930535c1c1273936d
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-06 12:16:53 +0200

    format code propre

--- src/main/java/com/mycompany/invoise/core/repository/InvoiceRepositoryInterface.java
+++ src/main/java/com/mycompany/invoise/core/repository/InvoiceRepositoryInterface.java
@@ -4,11 +4,8 @@ import com.mycompany.invoise.core.entity.Invoice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
-
-import java.util.List;
-
-public interface InvoiceRepositoryInterface extends CrudRepository<Invoice,String> {
-    @EntityGraph(value="invoice.customer",type= EntityGraph.EntityGraphType.FETCH)
+public interface InvoiceRepositoryInterface extends CrudRepository<Invoice, String> {
+    @EntityGraph(value = "invoice.customer", type = EntityGraph.EntityGraphType.FETCH)
     Iterable<Invoice> findAll();
     /*Invoice create(Invoice invoice);
     List<Invoice> list();
