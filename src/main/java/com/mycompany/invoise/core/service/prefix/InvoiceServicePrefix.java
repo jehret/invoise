9945d836fec5fd42e6d36b78aca88eb63edf93c5
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-07 15:47:16 +0200

    format code propre

--- src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
@@ -28,8 +28,8 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface {
         this.invoiceRepository = invoiceRepository;
     }
 
-    public void createInvoice(Invoice invoice){
-        invoice.setNumber(prefix+(++lastNumber));
+    public void createInvoice(Invoice invoice) {
+        invoice.setNumber(prefix + (++lastNumber));
         invoiceRepository.create(invoice);
     }
 
