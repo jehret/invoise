00953f7609140f26b75d5c9ec174dc5867aa6fb2
Jean-Philippe Ehret <jeanphiehret@gmail.com>
2024-10-20 16:32:09 +1300

    Ajustement invoice number

--- src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
+++ src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
@@ -3,10 +3,11 @@ package com.mycompany.invoise.core.service.number;
 import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
+@Service
 public class InvoiceServiceNumber implements InvoiceServiceInterface {
 
     private static long lastNumber = 0L;
