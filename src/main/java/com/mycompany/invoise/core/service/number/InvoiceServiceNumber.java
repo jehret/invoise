99538a37ae2e184e4ac6a9ed85d1dc65185f03fd
Edouard Leroy <leroyedouard@protonmail.com>
2024-10-20 11:30:54 +0200

    injection dependance (autowire) dans nouveau service

--- src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
+++ src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
@@ -3,6 +3,7 @@ package com.mycompany.invoise.core.service.number;
 import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class InvoiceServiceNumber implements InvoiceServiceInterface {
 
     private static long lastNumber = 0L;
 
+    @Autowired
     private InvoiceRepositoryInterface invoiceRepository;
 
     @Override
