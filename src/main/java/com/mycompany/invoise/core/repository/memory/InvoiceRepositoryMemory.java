40aea2d8e68029f64499953f8aea8ad0bd9afac2
Edouard Leroy <leroyedouard@protonmail.com>
2024-10-24 22:51:29 +0200

    maj InvoiceRepositoryMemory

--- src/main/java/com/mycompany/invoise/core/repository/memory/InvoiceRepositoryMemory.java
+++ src/main/java/com/mycompany/invoise/core/repository/memory/InvoiceRepositoryMemory.java
@@ -23,20 +23,8 @@ public class InvoiceRepositoryMemory implements InvoiceRepositoryInterface {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns all instances of the type {@code T} with the given IDs.
-     * <p>
-     * If some or all ids are not found, no entities are returned for these IDs.
-     * <p>
-     * Note that the order of elements in the result is not guaranteed.
-     *
-     * @param longs must not be {@literal null} nor contain any {@literal null} values.
-     * @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
-     * {@literal ids}.
-     * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
-     */
     @Override
-    public Iterable<Invoice> findAllById(Iterable<Long> longs) {
+    public Iterable<Invoice> findAllById(Iterable<Long> iterable) {
         throw new UnsupportedOperationException();
     }
 
@@ -45,21 +33,14 @@ public class InvoiceRepositoryMemory implements InvoiceRepositoryInterface {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Deletes the entity with the given id.
-     * <p>
-     * If the entity is not found in the persistence store it is silently ignored.
-     *
-     * @param aLong must not be {@literal null}.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
-     */
     @Override
-    public void deleteById(Long aLong) {
-
+    public void deleteById(Long s) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void delete(Invoice invoice) {
+        throw new UnsupportedOperationException();
     }
 
     /**
@@ -73,7 +54,18 @@ public class InvoiceRepositoryMemory implements InvoiceRepositoryInterface {
      */
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
+
     }
+
+    /**
+     * Deletes all instances of the type {@code T} with the given IDs.
+     * <p>
+     * Entities that aren't found in the persistence store are silently ignored.
+     *
+     * @param iterable must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
+     * @since 2.5
+     */
 
     @Override
     public void deleteAll(Iterable<? extends Invoice> iterable) {
@@ -90,27 +82,15 @@ public class InvoiceRepositoryMemory implements InvoiceRepositoryInterface {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Retrieves an entity by its id.
-     *
-     * @param aLong must not be {@literal null}.
-     * @return the entity with the given id or {@literal Optional#empty()} if none found.
-     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-     */
+
     @Override
-    public Optional<Invoice> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Invoice> findById(Long s) {
+        throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns whether an entity with the given id exists.
-     *
-     * @param aLong must not be {@literal null}.
-     * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-     */
+
     @Override
-    public boolean existsById(Long aLong) {
-        return false;
+    public boolean existsById(Long s) {
+        throw new UnsupportedOperationException();
     }
 }
