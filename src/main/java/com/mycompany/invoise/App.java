cd0727772760316c981b2522ddb249fa33d8d8a0
Jean-Philippe Ehret <jeanphiehret@gmail.com>
2024-11-08 15:29:43 +1300

    Spring 6

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,19 +1,8 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
-import com.mycompany.invoise.controller.InvoiceControllerKeyboard;
-import com.mycompany.invoise.controller.InvoiceControllerDouchette;
-import com.mycompany.invoise.controller.InvoiceControllerWeb;
-import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
-import com.mycompany.invoise.repository.InvoiceRepositoryMemory;
-import com.mycompany.invoise.repository.InvoiceRepositoryDatabase;
-import com.mycompany.invoise.service.InvoiceServiceInterface;
-import com.mycompany.invoise.service.InvoiceServiceNumber;
-import com.mycompany.invoise.service.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Scanner;
 
 /**
  * Hello world!
